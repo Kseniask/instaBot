0346d00a1baa1ce9628bb80ea927aa2351672821
kseniask <kseniask30@icloud.com>
2022-11-08 00:03:40 -0800

    updated implementation

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
-const { TELEGRAM_TOKEN, BASE_API_URL } = process.env;
+const { TELEGRAM_TOKEN } = process.env;
 const bot = new Telegraf(TELEGRAM_TOKEN);
 const axios = require('axios');
-const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
-const request_client = require('request-promise-native');
 
 const introMessage = 'Привіт крисятко. Велкам в світ, де можна дивитись сторі бившого анонімно!';
 const helpMessage =
@@ -27,46 +25,78 @@ bot.mention(async (ctx) => {
   const instaUsername = ctx.update.message.text.slice(1);
   ctx.reply(randomPhrases[(Math.random() * randomPhrases.length) | 0]);
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 
   try {
     const page = await browser.newPage();
-    await page.goto(`https://instastories.watch/en/${instaUsername}/`, { waitUntil: 'networkidle0' });
-    await page.goto(`https://instastories.watch/api/profile?username=${instaUsername}`, {
-      waitUntil: 'networkidle0'
+    await page.setRequestInterception(true);
+    let userData;
+    page.on('requestfinished', async (request) => {
+      if (request.url().includes(`https://www.instagram.com/api/v1/users`)) {
+        userData = (await request.response().json()).data.user;
+      }
+    });
+    let stopRequests = false;
+    page.on('request', (request) => {
+      if (request.url().includes(`https://www.instagram.com/api/v1/users`)) {
+        stopRequests = true;
+        return request.continue();
+      }
+      if (stopRequests) {
+        return request.abort();
+      }
+      request.continue();
     });
 
-    const userData = await page.evaluate(() => JSON.parse(document.body.innerText).stories);
-    const stories = userData.filter((entry) => entry.type !== 'ads');
-    const mediaGroups = [[], [], [], []];
-    stories.forEach((story, index) => {
-      const mediaValue = { type: story.type, media: story.url.replace('/api/proxy/', '') };
-      if (index < 10) {
-        mediaGroups[0].push(mediaValue);
-      } else if (index < 20) {
-        mediaGroups[1].push(mediaValue);
-      } else if (index < 30) {
-        mediaGroups[2].push(mediaValue);
-      } else {
-        mediaGroups[3].push(mediaValue);
-      }
-      return;
-    });
-    mediaGroups.forEach((mediaGroup) => {
-      if (mediaGroup.length > 0) {
-        if (mediaGroup.length === 1) {
-          switch (mediaGroup[0].type) {
-            case 'photo':
-              bot.telegram.sendPhoto(ctx.chat.id, mediaGroup[0].media);
-              break;
-            case 'video':
-              bot.telegram.sendVideo(ctx.chat.id, mediaGroup[0].media);
-          }
+    await page.goto(`https://www.instagram.com/${instaUsername}/`, { waitUntil: 'networkidle0' });
+
+    if (userData.is_private) {
+      return ctx.reply('Упсі.. Акаунт то привaтний. Я не настільки кльовий, сорі ');
+    }
+
+    const userStories = await axios
+      .get(`https://storiesig.info/api/ig/stories/${userData.id}`)
+      .then((res) => res.data.result || undefined);
+
+    if (userStories.length !== 0) {
+      const mediaGroups = [[], [], [], []];
+      userStories.forEach((story, index) => {
+        const isVideo = story.video_versions;
+        const storyUrl = isVideo ? story.video_versions[0].url : story.image_versions2.candidates[0].url;
+
+        const mediaValue = {
+          type: isVideo ? 'video' : 'photo',
+          media: storyUrl
+        };
+        if (index < 10) {
+          mediaGroups[0].push(mediaValue);
+        } else if (index < 20) {
+          mediaGroups[1].push(mediaValue);
+        } else if (index < 30) {
+          mediaGroups[2].push(mediaValue);
         } else {
-          bot.telegram.sendMediaGroup(ctx.chat.id, mediaGroup);
+          mediaGroups[3].push(mediaValue);
         }
-      }
-    });
+        return;
+      });
+      mediaGroups.forEach((mediaGroup) => {
+        if (mediaGroup.length > 0) {
+          if (mediaGroup.length === 1) {
+            switch (mediaGroup[0].type) {
+              case 'photo':
+                bot.telegram.sendPhoto(ctx.chat.id, mediaGroup[0].media);
+                break;
+              case 'video':
+                bot.telegram.sendVideo(ctx.chat.id, mediaGroup[0].media);
+            }
+          } else {
+            bot.telegram.sendMediaGroup(ctx.chat.id, mediaGroup);
+          }
+        }
+      });
+    } else {
+      ctx.reply('Пусто');
+    }
   } catch (e) {
     console.log('Error occured: ', e.message);
     ctx.reply('Шось не то.. Виникла помилка');
