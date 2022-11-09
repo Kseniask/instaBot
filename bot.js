beb21b56cfc76693dd1bda46f7aad6ea2f184954
kseniask <kseniask30@icloud.com>
2022-11-08 23:17:05 -0800

    removed 5 sec delay

--- bot.js
+++ bot.js
@@ -40,7 +40,6 @@ bot.mention(async (ctx) => {
     let userData;
     page.on('requestfinished', async (request) => {
       if (request.url().includes(`https://www.instagram.com/api/v1/users`)) {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
         console.log('response', (await request.response().json()).data);
         userData = (await request.response().json()).data.user;
       }
